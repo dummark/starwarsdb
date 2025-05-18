575b2f7e9259e3c48b8dc79dee51b6e543ba978b
Mark Dumler <90216931+dummark@users.noreply.github.com>
2025-05-19 00:03:56 +0300

    random planet

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+	return (
+		<div>
+			<div className='loadingio-spinner-double-ring-2by998twmg8'>
+				<div className='ldio-yzaezf3dcmj'>
+					<div></div>
+					<div></div>
+					<div>
+						<div></div>
+					</div>
+					<div>
+						<div></div>
+					</div>
+				</div>
+			</div>
+			<style type='text/css'></style>
+		</div>
+	);
+};
+
+export default Spinner;
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,5 +1,5 @@
 export default class SwapiService {
-	_apiBase = 'https://www.swapi.tech/api';
+	_apiBase = 'https://swapi.py4e.com/api//';
 
 	async getResource(url) {
 		const res = await fetch(`${this._apiBase}${url}`);
@@ -16,8 +16,9 @@ export default class SwapiService {
 		return res.results;
 	}
 
-	getPerson(id) {
-		return this.getResource(`/people/${id}`);
+	async getPerson(id) {
+		const person = await this.getResource(`/people/${id}`);
+		return this._transformPlanet(person);
 	}
 
 	async getAllPlanets() {
@@ -25,24 +26,55 @@ export default class SwapiService {
 		return res.results;
 	}
 
-	getPlanet(id) {
-		return this.getResource(`/planets/${id}`);
+	async getPlanet(id) {
+		const planet = await this.getResource(`/planets/${id}`);
+		return this._transformPlanet(planet);
 	}
 
 	async getAllStarships() {
 		const res = await this.getResource(`/starships/`);
-		return res.results;
+		return res.results.map(this._transformPlanet);
 	}
 
-	getStarship(id) {
-		return this.getResource(`/starships/${id}`);
+	async getStarship(id) {
+		const starship = await this.getResource(`/starships/${id}`);
+		return this._transformPlanet(starship);
+	}
+
+	_extractId(item) {
+		const idRegExp = /\/([0-9]*)\/$/;
+		return item.url.match(idRegExp[1]);
+	}
+
+	_transformPlanet(planet) {
+		return {
+			id: this._extractId(planet),
+			name: planet.name,
+			population: planet.population,
+			rotationPeriod: planet.rotation_period,
+			diameter: planet.diameter,
+		};
+	}
+	_transformStarship(starship) {
+		return {
+			id: this._extractId(starship),
+			name: starship.name,
+			model: starship.model,
+			manufacturer: starship.manufacturer,
+			costInCredits: starship.costInCredits,
+			length: starship.length,
+			crew: starship.crew,
+			passengers: starship.passengers,
+			cargoCapacity: starship.cargoCapacity,
+		};
+	}
+	_transformPerson(person) {
+		return {
+			id: this._extractId(person),
+			name: person.name,
+			gender: person.gender,
+			birthYear: person.birthYear,
+			eyeColor: person.eyeColor,
+		};
 	}
 }
-
-const swapi = new SwapiService();
-
-swapi.getAllPeople().then(people => {
-	people.forEach(p => {
-		console.log(p.name);
-	});
-});
